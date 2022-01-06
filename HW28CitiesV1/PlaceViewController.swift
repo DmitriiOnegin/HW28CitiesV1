06410e45dcd5bd804550958275963a1cbb7f09ed
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-06 14:15:35 +0300

    Commet2

---
+++ HW28CitiesV1/PlaceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlaceViewController.swift
+//  HW28CitiesV1
+//
+//  Created by Dmitrii Onegin on 06.01.2022.
+//
+
+import UIKit
+
+class PlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    
+
+}
---
+++ HW28CitiesV1/TabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarViewController.swift
+//  HW28CitiesV1
+//
+//  Created by Dmitrii Onegin on 06.01.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
