f86b438c8665e808da7b1557c77f06b64b7d6638
Dmitry <radiator211@mail.ru>
2022-01-08 01:36:56 +0700

    Add VC

--- place.swift
+++ place.swift
@@ -1,8 +1,20 @@
 //
-//  place.swift
+//  PlaceModel.swift
 //  HW28CitiesV1
 //
-//  Created by Дмитрий Бессонов on 08.01.2022.
+//  Created by Dmitrii Onegin on 07.01.2022.
 //
 
 import Foundation
+
+struct Place {
+    
+    let name: String
+    let address: String
+    let rating: String
+    let city: String
+    let image: String
+    let discription: String
+    let category: Categora
+    
+}
