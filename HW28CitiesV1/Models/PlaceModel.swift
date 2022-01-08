1de6e197f215492c10be19747f305669130b0176
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-08 14:29:17 +0300

    add DataManager & Models

---
+++ HW28CitiesV1/Models/PlaceModel.swift
@@ -0,0 +1,20 @@
+//
+//  PlaceModel.swift
+//  HW28CitiesV1
+//
+//  Created by Dmitrii Onegin on 07.01.2022.
+//
+
+import Foundation
+
+struct Place {
+    
+    let name: String
+    let address: String
+    let rating: String
+    let city: String
+    let image: String
+    let discription: String
+    let category: Categora
+    
+}
