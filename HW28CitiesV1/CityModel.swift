ee9a5275de6f584a85f83c49e6144832dd2917cc
Johnny Boshechka <in_order@icloud.com>
2022-01-09 02:03:40 +0500

    2:00 pm commit

--- HW28CitiesV1/CityModel.swift
+++ HW28CitiesV1/CityModel.swift
@@ -8,16 +8,73 @@
 import Foundation
 
 struct City {
-    let name: String
-    let image: String
-    let population: Float
-    let area: Float
-    let foundedDate: Int
-    
-    let sights: [Sight]
-    
-    static func getCity() -> City {
-        City(name: "Ufa", image: "1", population: 111, area: 47, foundedDate: 1934 , sights: Sight.getSights())
+    let usersCity: String
+    let userName: String
+    let population: String
+    let area: String
+    let foundedDate: String
+    let places: [Place]
+}
+
+extension City {
+    static func returnCityesArray() -> [City] {
+        let names = DataManager.shared.userNames
+        let cityes = DataManager.shared.cityNames
+        let populations = DataManager.shared.population
+        let areas = DataManager.shared.area
+        let foundedDates = DataManager.shared.foundedDate
+        let sights = DataManager.shared.sights
+        
+        var cityesArray: [City] = []
+        
+        for index in 0 ..< cityes.count {
+            var places =  [Place]()
+            var categira = Categora.theatre
+            for place in sights[index] {
+                switch place[5] {
+                case "театр":
+                    categira = .theatre
+                case "спорт":
+                    categira = .sport
+                case "архитектура":
+                    categira = .architecture
+                case "еда":
+                    categira = .food
+                case "парк":
+                    categira = .park
+                default:
+                    break
+                }
+                
+                let newPlace = Place(name: place[0],
+                                     address: place[1],
+                                     rating: place[3],
+                                     city: place[2],
+                                     image: place[0],
+                                     discription: place[4],
+                                     category: categira)
+                places.append(newPlace)
+            }
+            
+            
+            let city = City(
+                usersCity: cityes[index],
+                userName: names[index],
+                population: populations[index],
+                area: areas[index],
+                foundedDate: foundedDates[index],
+                places: places
+            )
+            cityesArray.append(city)
+        }
+        // return cityesArray.sorted(by: <)
+        return cityesArray
+        
     }
 }
 
+//extension City: Comparable  {
+//    static func < (lhs: City, rhs: City) -> Bool {
+//        lhs.usersCity < rhs.usersCity
+//    }
+//}
--- HW28CitiesV1/CityViewController.swift
+++ HW28CitiesV1/CityViewController.swift
@@ -5,14 +5,13 @@
 //  Created by Johnny Boshechka on 1/7/22.
 //
 
-import Foundation
 import UIKit
 
+protocol TabBarViewControllerDelegate {
+    func setNewValues(for categirys: [Categora])
+}
+
 class CityViewController: UIViewController {
-    
-    let currentCity = City.getCity()
-    let sights = Sight.getSights()
-    let number: Int = 0
     
     @IBOutlet weak var currentCityView: UIImageView!
     @IBOutlet weak var populationLabel: UILabel!
@@ -20,37 +19,86 @@ class CityViewController: UIViewController {
     @IBOutlet weak var foundedDateLabel: UILabel!
     @IBOutlet weak var sightsTableView: UITableView!
     
+    var city: [City] = City.returnCityesArray()
+    var categirys: [Categora] = [.theatre, .sport, .architecture, .food, .park]
+    var sortedPlace: [Place] = []
+//    var delegate: SettingsViewControllerDelegate!
+    
+    //private var categirys: [Categora] = [.theatre, .sport, .architecture, .food, .park]
     override func viewDidLoad() {
         super.viewDidLoad()
+        getSortedPlace()
         setupCity()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        //        delegate.setNewValues(for: categirys)
+        //        print(self.categirys)
+    
+    }
+    
+    private func getSortedPlace() {
+        for categiry in categirys {
+            for place in city[4].places {
+                if categiry == place.category {
+                    sortedPlace.append(place)
+                }
+            }
+        }
+    }
+    
     private func setupCity() {
-        navigationItem.title = currentCity.name
-        currentCityView.image = UIImage(named: currentCity.image)
-        populationLabel.text = String(currentCity.population)
-        areaLabel.text = String(currentCity.area)
-        foundedDateLabel.text = String(currentCity.foundedDate)
-    }
-}
-
-extension CityViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        sights.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        navigationItem.title = city[4].usersCity
+        currentCityView.image = UIImage(named: city[4].usersCity)
+        populationLabel.text = String(city[4].population)
+        areaLabel.text = String(city[4].area)
+        foundedDateLabel.text = String(city[4].foundedDate)
+        // MARK: - Navigation
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        let sight = sights[indexPath.row]
-        
-        var content = cell.defaultContentConfiguration()
-        content.image = UIImage(named: sight.image)
-        content.text = sight.name
-        cell.contentConfiguration = content
-        return cell
         
     }
 }
+    extension CityViewController: UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // delegate.setNewValues(for: categirys)
+            categirys.count
+            // city.places.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath)
+            
+            var content = cell.defaultContentConfiguration()
+            
+            content.text = sortedPlace[indexPath.row].name
+            content.image = UIImage(named: sortedPlace[indexPath.row].name)
+            cell.contentConfiguration = content
+            
+            return cell
+        }
+        
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            
+            
+        }
+        
+        
+    }
+    
+    //extension CityViewController: SettingsViewControllerDelegate {
+    //    func setNewValues(for categirys: [Categora]) {
+    //
+    //        self.categirys = categirys
+    //
+    //    }
+    //}
+    //extension CityViewController: TabBarViewControllerDelegate {
+    //    func setNewValues(for categirys: [Categora]) {
+    //
+    //        self.categirys = categirys
+    //
+    //    }
+    //}
