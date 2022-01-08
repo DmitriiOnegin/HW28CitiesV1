f91b5ff3e9019d263958f18993654c9a2a13e426
Dmitry <radiator211@mail.ru>
2022-01-09 00:24:42 +0700

    Delete testing code and label

--- HW28CitiesV1/SettingsViewController.swift
+++ HW28CitiesV1/SettingsViewController.swift
@@ -24,64 +24,39 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var architectureLabel: UILabel!
     @IBOutlet weak var foodLabel: UILabel!
     @IBOutlet weak var testLabel: UILabel!
-    
-
-//    var sights = ["Театры", "Спорт", "Архитектура", "Еда", "Парки"]
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        testLabel.text = sights.joined(separator: ", ")
-       // delegate.setNewValues(for: categirys)
+        
+        delegate.setNewValues(for: categirys)
     }
 
-    // по умолчанию все свичи включены и все достопримечательности открыты
-        // Когда к нам попадает массив - очищаем его, после этого, при включении свича, в массив добавляется категория
 
     @IBAction func switchAction() {
-//        sights.removeAll()
+
         categirys.removeAll()
         
         if theatersSwitch.isOn {
             categirys.append(.theatre)
-//            guard let theaters = theatersLabel.text else { return }
-//            sights.append(theaters)
         }
         if sportSwitch.isOn {
             categirys.append(.sport)
-//            guard let sport = sportLabel.text else { return }
-//            sights.append(sport)
+
         }
         if architectureSwitch.isOn {
             categirys.append(.architecture)
-//            guard let architecture = architectureLabel.text else { return }
-//            sights.append(architecture)
+
         }
         if foodSwitch.isOn {
             categirys.append(.food)
-//            guard let food = foodLabel.text else { return }
-//            sights.append(food)
+
         }
         if parksSwitch.isOn {
             categirys.append(.park)
-//            guard let parks = parksLabel.text else { return }
-//            sights.append(parks)
+
         }
-//        testLabel.text = sights.joined(separator: ", ")
         }
     }
     
- 
-    
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let tabBarController = segue.destination as? UITabBarController else {return}
-//        guard let viewControllers = tabBarController.viewControllers else { return }
-//
-//        for viewController in viewControllers {
-//    if let _ = viewController as? _{
-//       code
-//    } else if let navigationVC = viewController as? UINavigationController {
-//        let _VC = navigationVC.topViewController as! _
-//        code
 
